5edb62c6c00098d3e93fbb36d947c49757054464
DianaMMartins <dian_marq_mart@hotmail.com>
2023-03-16 12:31:58 +0000

    improved submit comment form

--- src/components/comments/NewComment.jsx
+++ src/components/comments/NewComment.jsx
@@ -3,10 +3,11 @@ import { postCommentToReview } from "../../utils.js/apiCalls";
 import { useContext } from "react";
 import { UserContext } from "../../contexts/Users";
 
-export const NewComment = ( {review_id, setComments} ) => {
+export const NewComment = ({ review_id, setComments }) => {
   const [newComment, setNewComment] = useState("");
-  const [placeholder, setPlaceholder] = useState('Post a comment here ...')
+  const [placeholder, setPlaceholder] = useState("Post a comment here ...");
   const { user } = useContext(UserContext);
+  const [submitButton, setSubmitButton] = useState(true);
   const commentToPost = {
     body: newComment,
     username: user,
@@ -14,25 +15,48 @@ export const NewComment = ( {review_id, setComments} ) => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    postCommentToReview(review_id, commentToPost).then(({comment}) => {
-      setComments((currentComments) => {
-        return [comment, ...currentComments]
-      })
-    });
-    setNewComment('')
-    setPlaceholder('Your comment has been posted, thank you!')
+    console.log(commentToPost.body);
+    if (
+      commentToPost.username === undefined ||
+      commentToPost.username.username === ""
+    ) {
+      setSubmitButton(false);
+      setNewComment("");
+      setPlaceholder("Please sign in to leave a review!");
+    } else if (commentToPost.body === "") {
+      setPlaceholder("Please write something to post a comment");
+      setSubmitButton(true);
+    } else {
+      postCommentToReview(review_id, commentToPost).then(({ comment }) => {
+        setComments((currentComments) => {
+          setSubmitButton(false);
+          return [comment, ...currentComments];
+        });
+      });
+      setNewComment("");
+      setPlaceholder(
+        "Your comment has been posted, thank you! You can only leave one review!"
+      );
+    }
   };
 
   return (
     <form className="post-comment" onSubmit={handleSubmit}>
       <label htmlFor="new-comment">Comment on this review: </label>
-      <textarea
-        id="new-comment"
-        value={newComment}
-        placeholder={placeholder}
-        onChange={(event) => setNewComment(event.target.value)}
-      />
-      <button type="submit">Comment</button>
+      <p>
+        <textarea
+          id="new-comment"
+          value={newComment}
+          placeholder={placeholder}
+          disabled={!submitButton}
+          rows="10"
+          cols="50"
+          onChange={(event) => setNewComment(event.target.value)}
+        />
+      </p>
+      <button type="submit" disabled={!submitButton}>
+        Comment
+      </button>
     </form>
   );
 };
