a3fa96f2f8492f4ac464fbd938f4f5fc7d06f33c
DianaMMartins <dian_marq_mart@hotmail.com>
2023-03-17 15:28:36 +0000

    solved error for deployment

--- src/components/categories/Categories.jsx
+++ src/components/categories/Categories.jsx
@@ -1,8 +1,6 @@
 import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
 import { getCategories } from "../../utils.js/apiCalls";
 import { CategoryCard } from "./CategoryCard";
-import { CategoryPage } from "./CategoryPage";
 
 //displays all category cards that exist
 export const Categories = () => {
@@ -29,17 +27,11 @@ export const Categories = () => {
       ) : (
         <ul className="categories-list">
           {categories.map((eachCategory) => {
-            const capitalTitle =
-              eachCategory.slug.charAt(0).toUpperCase() +
-              eachCategory.slug.slice(1);
-            const title = {
-              title: capitalTitle.replace(/-/g, " "),
-              category: eachCategory,
-            };
             return (
-              
-                <CategoryCard eachCategory={eachCategory}                 key={eachCategory.slug}
-/>
+              <CategoryCard
+                eachCategory={eachCategory}
+                key={eachCategory.slug}
+              />
             );
           })}
         </ul>
