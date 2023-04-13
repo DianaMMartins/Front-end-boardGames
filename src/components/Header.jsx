6dd867f61d37a442cea5fa062bc64282b2e03d58
DianaMMartins <dian_marq_mart@hotmail.com>
2023-04-13 10:52:20 +0100

    added link in heder to homepage

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,16 @@
 import { NavBar } from "./NavBar";
-import diceLogo from '../images/dice-logo-color.png'
-import './Header.css'
+import diceLogo from "../images/dice-logo-color.png";
+import "./Header.css";
+import { Link } from "react-router-dom";
 
 export const Header = () => {
   return (
-    <header className="Header">
-      <img src={diceLogo} alt="logo" width='50vw'/>
-      <h1>Board Game Reviews</h1>
-      <NavBar />
+    <header className="header">
+      <Link to={"/"}>
+        <img src={diceLogo} alt="logo" width="50vw" />
+        <h1>Board Game Reviews</h1>
+        <NavBar />
+      </Link>
     </header>
   );
 };
