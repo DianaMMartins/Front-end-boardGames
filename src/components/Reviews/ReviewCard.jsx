47bfcd5c25c4ba9e0a26d38006f4aa1b32983598
DianaMMartins <dian_marq_mart@hotmail.com>
2023-04-13 14:47:20 +0100

    improved hover function for reviews

--- src/components/Reviews/ReviewCard.jsx
+++ src/components/Reviews/ReviewCard.jsx
@@ -3,8 +3,10 @@ export const ReviewCard = ({ eachReview }) => {
   
     <li className="review-card" key={eachReview.review_id}>
       <img src={eachReview.review_img_url} alt={`${eachReview.title}`} />
+      <div className="review-intro">
       <h3 className="game-title">{eachReview.title}</h3>
       <p className="category">{eachReview.category}</p>
+      </div>
     </li>
   )
 };
