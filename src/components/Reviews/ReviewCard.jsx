c1bd0b06ae05fb35d7c517cd4b1d5e5ddfa92809
DianaMMartins <dian_marq_mart@hotmail.com>
2023-04-21 17:00:27 +0100

    removed console log

--- src/components/Reviews/ReviewCard.jsx
+++ src/components/Reviews/ReviewCard.jsx
@@ -1,6 +1,5 @@
 import './ReviewCard.css'
 export const ReviewCard = ({ eachReview }) => {
-  console.log(eachReview);
   return (
     <li className="review-card" key={eachReview.review_id}>
       <img src={eachReview.review_img_url} alt={`${eachReview.title}`} />
