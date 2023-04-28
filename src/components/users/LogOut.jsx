e84852429a0dcfda366dc1359877c8fe13459fe5
DianaMMartins <dian_marq_mart@hotmail.com>
2023-04-28 17:11:15 +0100

    improved css elements

--- src/components/users/LogOut.jsx
+++ src/components/users/LogOut.jsx
@@ -4,11 +4,11 @@ import "./LogOut.css";
 export const LogOut = () => {
   return (
     <section className="logout-page">
-      <h2>You have successfully logged out</h2>
+      <h2>You have successfully signed out</h2>
       <h3>See you next time!</h3>
       <Link to={`/users`}>
         <button>
-          <p>Back to login</p>
+          <p>Sign in</p>
         </button>
       </Link>
     </section>
