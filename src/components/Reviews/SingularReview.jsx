b286453ee7b4e59be42f0b422d4fe4e8a5923b5d
DianaMMartins <dian_marq_mart@hotmail.com>
2023-04-28 10:32:12 +0100

    implemented comment like button

--- src/components/Reviews/SingularReview.jsx
+++ src/components/Reviews/SingularReview.jsx
@@ -1,18 +1,15 @@
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { getReviewByReviewId, patchReviewVotes } from "../../utils/apiCalls";
 import { useParams } from "react-router-dom";
 import "./SingularReview.css";
 import { Comments } from "../comments/Comments";
-import heart from "../../images/heart.png";
-import noHeart from "../../images/heart-empty.png";
+import { UpVoteButton } from "../../utils/upVote";
 
 export const SingularReview = () => {
   const { review_id } = useParams();
   const [isLoading, setIsLoading] = useState(false);
-  const [voteButton, setVoteButton] = useState(false);
   const [singularReview, setSingularReview] = useState({});
-  const [like, setLike] = useState(noHeart);
-  const [error, setError] = useState(null);
+  const [error] = useState(null);
   const {
     title,
     designer,
@@ -32,30 +29,6 @@ export const SingularReview = () => {
     };
     fetchReview();
   }, [review_id]);
-
-  const upVote = useCallback(async () => {
-    setIsLoading(true);
-    try {
-      if (voteButton === false) {
-        setVoteButton(true);
-        setLike(heart);
-        singularReview.votes += 1;
-        await patchReviewVotes(review_id, 1);
-      } else {
-        setVoteButton(false);
-        setLike(noHeart);
-        singularReview.votes -= 1;
-        await patchReviewVotes(review_id, -1);
-      }
-    } catch (error) {
-      console.error(error);
-      setError("Error updating review.");
-    } finally {
-      setIsLoading(false);
-    }
-  }, [review_id, voteButton, singularReview]);
-
-  console.log(like);
 
   return (
     <section className="review-page">
@@ -92,13 +65,13 @@ export const SingularReview = () => {
               </section>
 
               <section className="vote-button">
-                <button onClick={() => upVote(review_id)}>
-                  <span aria-label="votes for this review">
-                    {votes + " "}
-                    {<img id='heart' src={like} alt="heart" />}
-                  </span>
-                </button>
+                <UpVoteButton
+                  patchVotes={patchReviewVotes}
+                  itemToPatch={votes}
+                  component_id={review_id}
+                />
               </section>
+
             </section>
             <Comments review_id={review_id} />
           </section>
