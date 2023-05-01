d9b638a3eb1f4a071fafeec3239ed80fdd43b612
DianaMMartins <dian_marq_mart@hotmail.com>
2023-05-01 15:28:53 +0100

    improved footer

--- src/components/Homepage/Footer.jsx
+++ src/components/Homepage/Footer.jsx
@@ -1,33 +1,31 @@
-import "./Footer.css";
 import { ScrollToTop } from "./ScrollToTop";
+import linkedin from "../../images/linkedin.png";
+import github from "../../images/github.png";
+import "./Footer.css";
 
 export const Footer = () => {
   return (
     <footer className="footer">
       <p className="text-footer">By Diana Martins</p>
-      <a
-        className="link-card"
-        target="_blank"
-        rel="noreferrer"
-        href="https://www.linkedin.com/in/diana-martins/"
-      >
-        <img
-          alt="linked in link"
-          src="https://www.freepnglogos.com/uploads/linkedin-logo-black-png-12.png"
-        />
-      </a>
-      <a
-        className="link-card"
-        target="_blank"
-        rel="noreferrer"
-        href="https://github.com/DianaMMartins"
-      >
-        <img
-          alt="github link"
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Github-circle_%28CoreUI_Icons_v1.0.0%29.svg/2048px-Github-circle_%28CoreUI_Icons_v1.0.0%29.svg.png"
-        />
-      </a>
-      <ScrollToTop />
+      <section className="links">
+        <a
+          className="link-card"
+          target="_blank"
+          rel="noreferrer"
+          href="https://www.linkedin.com/in/diana-martins/"
+        >
+          <img alt="linked in link" src={linkedin} />
+        </a>
+        <a
+          className="link-card"
+          target="_blank"
+          rel="noreferrer"
+          href="https://github.com/DianaMMartins"
+        >
+          <img alt="github link" src={github} />
+        </a>
+        <ScrollToTop />
+      </section>
     </footer>
   );
 };
